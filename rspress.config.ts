394fb20d129591ed6104d7379c64f7473b797a37
whatap-docs <kj.im@whatap.io>
2024-06-28 16:50:52 +0900

    create project

---
+++ rspress.config.ts
@@ -0,0 +1,21 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'WhaTap Docs',
+  description: '와탭 기술 문서 페이지에 오신 것을 진심으로 환영합니다.',
+  icon: '/rspress-icon.png',
+  logo: {
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
+  },
+  markdown: {
+    mdxRs: false
+  },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+    ],
+  },
+});
